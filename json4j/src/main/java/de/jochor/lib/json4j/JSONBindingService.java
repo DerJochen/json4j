b92bc4b58a3f4bf11494f7eabfc62ec4c8e3aab0
Jochen Hormes <jochen@jochor.de>
2015-09-22 10:26:19 +0200

    json4j: Added JavaDoc comments.

--- json4j/src/main/java/de/jochor/lib/json4j/JSONBindingService.java
+++ json4j/src/main/java/de/jochor/lib/json4j/JSONBindingService.java
@@ -1,15 +1,27 @@
 package de.jochor.lib.json4j;
 
 /**
- *
+ * Interface for JSON Binding Service adapter implementations.
+ * 
  * @author Jochen Hormes
  * @start 2015-08-24
  *
  */
 public interface JSONBindingService {
 
+	/**
+	 * Converts a JSON String to an entity of the given type.
+	 * @param json String representation
+	 * @param entityType Type to convert to
+	 * @return Entity instance
+	 */
 	<T>T toEntity(String json, Class<T> entityType);
 
+	/**
+	 * Serializes the given entity to JSON.
+	 * @param entity Source entity
+	 * @return JSON representation
+	 */
 	String toJSON(Object entity);
 
 }
--- json4j/src/main/java/de/jochor/lib/json4j/JSONBindingServiceFactory.java
+++ json4j/src/main/java/de/jochor/lib/json4j/JSONBindingServiceFactory.java
@@ -3,7 +3,8 @@ package de.jochor.lib.json4j;
 import de.jochor.lib.servicebuilder.ServiceFactory;
 
 /**
- *
+ * Factory for JSON Binding Service adapters. Use the static {@link #create()} method to retrieve an instance.
+ * 
  * @author Jochen Hormes
  * @start 2015-08-26
  *
@@ -12,6 +13,11 @@ public class JSONBindingServiceFactory extends ServiceFactory {
 
 	private static final String BINDER_NAME = "de/jochor/lib/json4j/StaticJSONBindingBinder.java";
 
+	/**
+	 * Factory method to produce a new instance of a JSON Binding Service adapter.
+	 * 
+	 * @return Service instance
+	 */
 	public static JSONBindingService create() {
 		JSONBindingService httpClient = create(BINDER_NAME);
 		return httpClient;
